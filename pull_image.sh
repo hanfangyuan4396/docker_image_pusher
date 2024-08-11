63226db3e23c5abf417aba313f4a90601219f67b
Han Fangyuan <hanfangyuan_ustb@163.com>
2024-08-11 15:54:58 +0800

    fix: unbuntu squid

--- pull_image.sh
+++ pull_image.sh
@@ -18,7 +18,7 @@ sudo docker tag ${acr_image_prefix}/postgres:15-alpine postgres:15-alpine
 sudo docker tag ${acr_image_prefix}/redis:6-alpine redis:6-alpine
 sudo docker tag ${acr_image_prefix}/weaviate:1.19.0 semitechnologies/weaviate:1.19.0
 sudo docker tag ${acr_image_prefix}/dify-sandbox:0.2.1 langgenius/dify-sandbox:0.2.1
-sudo docker tag ${acr_image_prefix}/squid:latest sameersbn/squid:latest
+sudo docker tag ${acr_image_prefix}/squid:latest ubuntu/squid:latest
 sudo docker tag ${acr_image_prefix}/nginx:latest nginx:latest
 sudo docker tag ${acr_image_prefix}/dify-api:0.6.16 langgenius/dify-api:0.6.16
 sudo docker tag ${acr_image_prefix}/dify-web:0.6.16 langgenius/dify-web:0.6.16
